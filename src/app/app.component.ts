f5e37ea6b5f13d14e0c9752da8d429c842662b1a
rcortes <rcortes@kys.cl>
2020-05-24 19:36:28 -0400

    src/app/app.component.ts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'site-angular-aws';
+  title = 'Angular 9 - Funciones Lambda';
+
+private SERVER_URL = "https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS";
+constructor(private httpClient: HttpClient) { }
+
+lambda = this.httpClient.get('${this.SERVER_URL}');
+
 }
