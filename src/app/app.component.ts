ed88a6547bdf7dcbe9e0b41cc04412e28a98a795
rcortes <rcortes@kys.cl>
2020-05-25 11:17:26 -0400

    src/app/app.component.ts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({ 
+    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin':'*'
   })
 };
@@ -12,19 +13,37 @@ const httpOptions = {
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular 9 - Funciones Lambda';
+  lambda: any = null;
+constructor(private http: HttpClient) { }
 
-constructor(private httpClient: HttpClient) { }
-
-lambda = this.httpClient.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
-.subscribe (
-  response => {
-    console.log(response);
-  },
-  error => {
-    console.log(error);
-  }
-);
+ngOnInit() {
+  this.http.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
+  .toPromise()
+      .then(data=>{
+        let res = {'results': JSON.stringify(data),
+        'json': ()=>{return data;}
+      };
+        this.lambda = res.results;
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+}
 
 }
+
+/*
+this.httpClient.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
+.toPromise()
+.then(data=>{
+  let res = {'results': JSON.stringify(data),
+  'json': ()=>{return data;}
+};
+ return res; 
+})
+.catch(error => {
+  return Promise.reject(error);
+});
+*/
