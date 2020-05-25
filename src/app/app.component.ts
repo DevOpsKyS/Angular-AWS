0bfa7f797dd12573d75a6300d3f2500e14881fc0
rcortes <rcortes@kys.cl>
2020-05-24 22:30:40 -0400

    src/app/app.component.ts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*'
+  })
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Angular 9 - Funciones Lambda';
 
 constructor(private httpClient: HttpClient) { }
 
-lambda = this.httpClient.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS')
+lambda = this.httpClient.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
 .subscribe (
   response => {
     console.log(response);
