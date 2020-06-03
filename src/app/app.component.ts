47cd20d3d00e270616f18ac9bf05d3b8993d833b
rcortes <rcortes@kys.cl>
2020-06-03 19:47:25 -0400

    amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,34 +16,19 @@ const httpOptions = {
 export class AppComponent implements OnInit {
   title = 'Angular 9 - Funciones Lambda';
   lambda: any = null;
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-ngOnInit() {
-  this.http.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
-  .toPromise()
-      .then(data=>{
-        let res = {'results': JSON.stringify(data),
-        'json': ()=>{return data;}
-      };
-        this.lambda = res.results;
-      })
-      .catch(error => {
-        return Promise.reject(error);
-      });
+  ngOnInit() {
+    this.http.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
+    .toPromise()
+        .then(data=>{
+          let res = {'results': JSON.stringify(data),
+          'json': ()=>{return data;}
+        };
+          this.lambda = res.results;
+        })
+        .catch(error => {
+          return Promise.reject(error);
+        });
+  }
 }
-
-}
-
-/*
-this.httpClient.get('https://bn86do16pc.execute-api.us-east-2.amazonaws.com/default/pythonKYS', httpOptions)
-.toPromise()
-.then(data=>{
-  let res = {'results': JSON.stringify(data),
-  'json': ()=>{return data;}
-};
- return res; 
-})
-.catch(error => {
-  return Promise.reject(error);
-});
-*/
